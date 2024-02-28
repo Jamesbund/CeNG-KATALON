7f78590574769529fdb89b9f015401f007f4543a
Jamesbund <115215180+Jamesbund@users.noreply.github.com>
2024-02-28 17:05:43 +0800

    CeNG Web Automation V1.0.2_02282024
    Changes: Added Test Suites Collection

---
+++ CeNG Web Automation V1.02_02282024/Libs/TempTestSuite1709108232801.groovy
@@ -0,0 +1,36 @@
+import com.kms.katalon.core.logging.KeywordLogger
+import com.kms.katalon.core.exception.StepFailedException
+import com.kms.katalon.core.reporting.ReportUtil
+import com.kms.katalon.core.main.TestCaseMain
+import com.kms.katalon.core.testdata.TestDataColumn
+import com.kms.katalon.core.testcase.TestCaseBinding
+import com.kms.katalon.core.driver.internal.DriverCleanerCollector
+import com.kms.katalon.core.model.FailureHandling
+import com.kms.katalon.core.configuration.RunConfiguration
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import internal.GlobalVariable as GlobalVariable
+
+Map<String, String> suiteProperties = new HashMap<String, String>();
+
+suiteProperties.put('id', 'Test Suites/CeNG Client Creation Test Suites')
+suiteProperties.put('name', 'CeNG Client Creation Test Suites')
+suiteProperties.put('description', 'Test Suites for Client Creation')
+suiteProperties.put('rerunTestFailImmediately', 'false')
+suiteProperties.put('retryCount', '0')
+ 
+
+DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.webui.contribution.WebUiDriverCleaner())
+DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.mobile.contribution.MobileDriverCleaner())
+DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.cucumber.keyword.internal.CucumberDriverCleaner())
+DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.windows.keyword.contribution.WindowsDriverCleaner())
+DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.testng.keyword.internal.TestNGDriverCleaner())
+
+
+
+RunConfiguration.setExecutionSettingFile("C:\\Users\\ja.bundalian\\Katalon Studio\\CeNG Web Automation\\Reports\\20240228_161712\\CeNG Client Creation Test Suites\\20240228_161712\\execution.properties")
+
+TestCaseMain.beforeStart()
+
+TestCaseMain.startTestSuite('Test Suites/CeNG Client Creation Test Suites', suiteProperties, new File("C:\\Users\\ja.bundalian\\Katalon Studio\\CeNG Web Automation\\Reports\\20240228_161712\\CeNG Client Creation Test Suites\\20240228_161712\\testCaseBinding"))
---
+++ CeNG Web Automation V1.02_02282024/Libs/internal/GlobalVariable.groovy
@@ -0,0 +1,54 @@
+package internal
+
+import com.kms.katalon.core.configuration.RunConfiguration
+import com.kms.katalon.core.main.TestCaseMain
+
+
+/**
+ * This class is generated automatically by Katalon Studio and should not be modified or deleted.
+ */
+public class GlobalVariable {
+     
+    /**
+     * <p></p>
+     */
+    public static Object UsernameTeller
+     
+    /**
+     * <p></p>
+     */
+    public static Object ClientFirstName
+     
+    /**
+     * <p></p>
+     */
+    public static Object ClientMiddleName
+     
+    /**
+     * <p></p>
+     */
+    public static Object ClientLastName
+     
+    /**
+     * <p></p>
+     */
+    public static Object PasswordTeller
+     
+
+    static {
+        try {
+            def selectedVariables = TestCaseMain.getGlobalVariables("default")
+			selectedVariables += TestCaseMain.getGlobalVariables(RunConfiguration.getExecutionProfile())
+            selectedVariables += TestCaseMain.getParsedValues(RunConfiguration.getOverridingParameters(), selectedVariables)
+    
+            UsernameTeller = selectedVariables['UsernameTeller']
+            ClientFirstName = selectedVariables['ClientFirstName']
+            ClientMiddleName = selectedVariables['ClientMiddleName']
+            ClientLastName = selectedVariables['ClientLastName']
+            PasswordTeller = selectedVariables['PasswordTeller']
+            
+        } catch (Exception e) {
+            TestCaseMain.logGlobalVariableError(e)
+        }
+    }
+}
